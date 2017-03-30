a2008fea8dd12ae56f68cac6fb1a7ec2a24e129c
Nathan Ryan <nathan.ryan26@gmail.com>
2017-03-30 14:37:27 +0100

    initial class commit

---
+++ src/main/java/com/distributed/printer/PrinterService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.distributed.printer;
+
+/**
+ * PrinterService.java
+ * @author Nathan Ryan x13448212 BSHC Mobile Stream
+ */
+public class PrinterService {
+    
+}
