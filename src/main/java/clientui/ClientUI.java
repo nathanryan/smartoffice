d865cf93d0032ba9c8b92031975bc16c1326ecee
Nathan Ryan <nathan.ryan26@gmail.com>
2017-04-14 11:29:38 +0100

    new structure

---
+++ src/main/java/clientui/ClientUI.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientui;
+
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author nathan
+ */
+public abstract class ClientUI extends JPanel implements ActionListener {
+    
+}
